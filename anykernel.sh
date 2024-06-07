8ec16f848fc32428ebe563af3679ae7d9a47c465
Fakhri Djuanda <41115223+fskhri@users.noreply.github.com>
2024-06-08 05:33:12 +0700

    Ribka

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Erine Kernel | POCO X3/NFC
+kernel.string=Ribka Kernel | POCO X3/NFC
 do.devicecheck=1
 do.modules=0
 do.systemless=1
